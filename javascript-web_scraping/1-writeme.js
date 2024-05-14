31e9e2e37669dd5f3f0fa5933ea25d6df028a033
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-14 07:37:58 -0700

    writeme semi fix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const fpath = process.argv[2];
 const fcontent = process.argv[3];
 
-fs.writeFile
-(fpath, fcontent, 'utf8', (err) => {
+fs.writeFile(fpath, fcontent, 'utf8', (err) => {
   if (err) {
     console.error(err);
   }
