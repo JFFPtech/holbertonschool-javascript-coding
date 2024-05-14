939d02fbd71ae87c2dd55460df6f5ac7987042d4
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-14 06:58:30 -0700

    test for write file, 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const fpath = process.argv[3];
+
+function write_file(content) {
+    fs.writeFile(fpath, content, 'utf8', (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Content written to ${fpath}');
+        });
+}
