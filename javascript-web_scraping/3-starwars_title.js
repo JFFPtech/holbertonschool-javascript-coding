1dadd98b6808f763893d0008879355946aaf31e3
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-14 07:49:03 -0700

    fixed semistandard indents

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,12 +3,10 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) {
+request(url, { json: true }, (error, response, body) => {
+  if (error) {
     console.error('Error:', error);
-  } else if (Response.statusCode !== 200) {
-    console.error('Failed to fetch data from API');
-  } else {
-    console.log(body.title);
+    return;
   }
+  console.log(body.title);
 });
