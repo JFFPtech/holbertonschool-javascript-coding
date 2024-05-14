36ad9408519e37cae8aae568926c5da8d015988a
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-14 06:49:58 -0700

    test for 3 and 4

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, { json: true }, (err, res, body) => {
+    if (err) {
+        console.error('Error:', error);
+    } else if (Response.statusCode !== 200) {
+        console.error('Failed to fetch data from API');
+    } else {
+        console.log(body.title);
+    }
+});
