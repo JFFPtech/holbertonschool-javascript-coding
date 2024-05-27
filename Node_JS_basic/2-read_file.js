9f9099e9f0e515e7044c8bcb4f733e0b7ab9a19d
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-27 08:09:57 -0700

    passed eslint on 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,36 +1,36 @@
 const fs = require('fs');
 
 function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, { encoding: 'utf8' });
-        const lines =data.split('\n').filter(line => line);
+  try {
+    const data = fs.readFileSync(path, { encoding: 'utf8' });
+    const lines = data.split('\n').filter((line) => line);
 
-        if (lines.length === 0) {
-            throw new Error('No data to parse');
-        }
-
-        const students = lines.slice(1).map(line => {
-            const [firstName, field] = line.split(',');
-            return { firstName, field };
-        }).filter(student => student.firstName && student.field);
-
-        const totalStudents = students.length;
-        console.log(`Number of students: ${totalStudents}`);
-
-        const fieldSummary = students.reduce((acc, { firstName, field }) => {
-            if (!acc[field]) {
-                acc[field] = [];
-            }
-            acc[field].push(firstName);
-            return acc;
-        }, {});
-
-        Object.entries(fieldSummary).forEach(([field, names]) => {
-            console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-        });
-    } catch (error) {
-        throw new Error('Cannot load the database');
+    if (lines.length === 0) {
+      throw new Error('No data to parse');
     }
+
+    const students = lines.slice(1).map((line) => {
+      const [firstName, field] = line.split(',');
+      return { firstName, field };
+    }).filter((student) => student.firstName && student.field);
+
+    const totalStudents = students.length;
+    console.log(`Number of students: ${totalStudents}`);
+
+    const fieldSummary = students.reduce((acc, { firstName, field }) => {
+      if (!acc[field]) {
+        acc[field] = [];
+      }
+      acc[field].push(firstName);
+      return acc;
+    }, {});
+
+    Object.entries(fieldSummary).forEach(([field, names]) => {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
