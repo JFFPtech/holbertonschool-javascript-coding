c5e0faf3de54316cc50058dcbc77bf9d69835bc6
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-27 12:23:56 -0700

    created app controller class for static req

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -5,3 +5,17 @@ export const readDatabase = (path) => {
         fs.readFile(path, 'utf8', (err, data) => {
             if (err) {
                 reject('Cannot load the database');
+            } else {
+                const students = {};
+                data.split('\n').slice(1).forEach(line => {
+                    if (line.length > 0) {
+                        const [firstName, , , field] = line.split(',');
+                        if (!students[field]) students[field] = [];
+                        students[field].push(firstName.trim());
+                    }
+                });
+                resolve(students);
+            }
+        });
+    });
+};
