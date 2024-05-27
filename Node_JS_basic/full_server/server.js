665c283a9ac6906502f8793b6ad59833ee71b0ad
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-27 13:56:06 -0700

    passed eslint, everything works after rollbck

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const app = express();
 const port = 1245;
@@ -7,7 +7,7 @@ const port = 1245;
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
 
 export default app;
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,24 +1,22 @@
 import fs from 'fs';
 
-export const readDatabase = (path) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(new Error('Cannot load the database'));  // Use Error object for better stack tracing
-            } else {
-                const students = {};
-                data.split('\n').slice(1).forEach(line => {
-                    if (line) {
-                        const [firstname, , , field] = line.split(',');
-                        const cleanField = field.trim(); // Ensure field names are trimmed
-                        if (!students[cleanField]) {
-                            students[cleanField] = [];
-                        }
-                        students[cleanField].push(firstname.trim());
-                    }
-                });
-                resolve(students);
-            }
-        });
-    });
-};
+export const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database')); // Use Error object for better stack tracing
+    } else {
+      const students = {};
+      data.split('\n').slice(1).forEach((line) => {
+        if (line) {
+          const [firstname, , , field] = line.split(',');
+          const cleanField = field.trim(); // Ensure field names are trimmed
+          if (!students[cleanField]) {
+            students[cleanField] = [];
+          }
+          students[cleanField].push(firstname.trim());
+        }
+      });
+      resolve(students);
+    }
+  });
+});
