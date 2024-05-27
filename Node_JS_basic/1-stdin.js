ddfd869bb1123b8a96bf336613c01013e8d429ca
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-27 07:39:08 -0700

    input for bob check, output ok

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,3 +1,9 @@
 process.stdin.setEncoding('utf8');
 
 console.log("Welcome to Holberton School, what is your name?");
+process.stdin.on('data', function(data) {
+    const name = data.trim();
+    console.log("Your name is: " + name);
+    console.log("This important software is now closing");
+    process.exit();
+});
