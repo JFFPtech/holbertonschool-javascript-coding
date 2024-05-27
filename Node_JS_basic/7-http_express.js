355d220238a2192179d7d2ebb39a873e28222638
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-27 10:33:24 -0700

    attempt for 1st block

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -6,4 +6,15 @@ const databasePath = process.argv[2];
 
 async function readStudents(path) {
   try {
-    const data = await
+    const data = await fs.readFile(path, { encoding: 'utf8' });
+    const lines = data.split('\n').filter((line) => line);
+    if (lines.length <= 1) {
+      throw new Error('No data in file');
+    }
+
+    const students = lines.slice(1).map(line => {
+        const [firstName, , , field] = line.split(',').map(part => part.trim());
+        return { firstName, field };
+    }).filter(student => student.firstName && student.field);
+
+    }
