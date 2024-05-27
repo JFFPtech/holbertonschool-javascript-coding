2b6fc374156a6459ae05c5dab706748999ea5f5c
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-27 08:04:15 -0700

    extract student info and count for students

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,4 +8,12 @@ function countStudents(path) {
         if (lines.length === 0) {
             throw new Error('No data to parse');
         }
+
+        const students = lines.slice(1).map(line => {
+            const [firstName, field] = line.split(',');
+            return { firstName, field };
+        }).filter(student => student.firstName && student.field);
+
+        const totalStudents = students.length;
+        console.log(`Number of students: ${totalStudents}`);
 }
